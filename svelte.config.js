f8f8e020d72c486f016b79ca3aa9c4c1a544bb0f
Seo Jeonghun <didam00@naver.com>
2023-12-02 02:31:00 +0900

    deploy commit

--- svelte.config.js
+++ svelte.config.js
@@ -19,14 +19,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: {
-			build: {
-				outDir: 'public'
-			},
-			// server: {
-			// 	port: 5000
-			// }
-		}
 	}
 };
 
